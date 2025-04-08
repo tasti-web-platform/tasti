6fd5ec06283ca09a97c2ca246103422b6a5f7f3d
Burak Salça <salcaburak@gmail.com>
2025-04-08 14:54:18 +0300

    TailwindCss eklendi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/tasti-vite/', // ← BU ÇOK ÖNEMLİ
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
 })
