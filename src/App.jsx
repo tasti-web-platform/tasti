37b19e4ca6636f114255c32f0ccab33320aea038
Burak Salça <salcaburak@gmail.com>
2025-04-09 04:35:35 +0300

    Github.io konfigürasyonu yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import NavBar from './layouts/NavBar';
 import MainPage from './layouts/MainPage';
 import Footer from './layouts/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
 import TryDemo from './pages/TryDemo';
@@ -15,7 +15,7 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <NavBar />
       <MainPage>
@@ -30,7 +30,7 @@ function App() {
         </Routes>
       </MainPage>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
