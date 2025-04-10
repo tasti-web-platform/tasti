ece151146a1d7a8fa33c68690936bca8aad5bd96
Burak Salça <salcaburak@gmail.com>
2025-04-10 11:24:59 +0300

    Fix base path in Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/tasti-vite/' : '/',
+  base: '/tasti-vite/', 
   plugins: [react(), tailwindcss()],
   server: {
-    historyApiFallback: true, //Consoldaki 404 (Not Found) hatasını kaldırmak için
+    historyApiFallback: true,
   },
 })
